41ff89e654e47314aa4d408cf8add6c8c3274af2
lstndrms <lstndrms13@gmail.com>
2021-05-15 04:03:06 +0300

    56

--- test.c
+++ test.c
@@ -5,12 +5,43 @@
 #include "flist.h"
 
 
-double t(double x) {
+double at1(double x) {
     return x * x / 10 - x + 1;
 }
 
-double it(double x) {
+double at2(double x) {
+    return cos(x);
+}
+
+double at3(double x) {
+    return x * x;
+}
+
+double at4(double x) {
+    return 1/(x * x * x);
+}
+
+double at5(double x) {
+    return sin(3 * x);
+}
+
+double at6(double x) {
+    return log(x) / 3;
+}
+
+
+
+double it1(double x) {
     return x * x * sin(x) * sin(x) * sin(x);
+}
+
+
+double it2(double x) {
+    return x * sqrt(x * x + 1);
+}
+
+double it3(double x) {
+    return log(x)/(x*x);
 }
 
 double t1(double x) {
@@ -24,16 +55,57 @@ double t3(double x) {
 }
 
 void atest(void) {
-    printf("Root test\n");
-    double x = root(t, t3, 6.068, 10.0, 1e-5);
-    printf("function: x^2/10-x+1-cosx=0\n\troot: %lf; real root: 8.28697\n", x);
-    assert(fabs(x - 8.28697) < 1e-5);
-    printf("Root test: OK\n");
+    printf("Root test 1\n");
+    double x = root(at1, at2, 6.068, 10.0, 1e-5);
+    printf("function: x^2/10-x+1-cosx=0, EPS: 0.00001\nroot: %.5lf; real root: 8.28697\n", x);
+    printf("Root test 1: ");
+    if (fabs(x - 8.28697) < 1e-5)
+        printf("OK\n\n");
+    else
+        printf("WA\n\n");
+
+    printf("Root test 2\n");
+    x = root(at3, at4, 0.5, 1.5, 1e-4);
+    printf("function: x^2-1/x^3=0, EPS: 0.0001\nroot: %.4lf; real root: 1.0000\n", x);
+    printf("Root test 2: ");
+    if (fabs(x - 1.0000) < 1e-4)
+        printf("OK\n\n");
+    else
+        printf("WA\n\n");
+
+    printf("Root test 3\n");
+    x = root(at5, at6, 2, 2.5, 1e-3);
+    printf("function: sin(3x)-lnx/3=0, EPS: 0.001\nroot: %.3lf; real root: 2.182\n", x);
+    printf("Root test 3: ");
+    if (fabs(x - 2.1821) < 1e-3)
+        printf("OK\n\n");
+    else
+        printf("WA\n\n");
+
+
     printf("Integral test\n");
-    x = integral(it, 0.0, 1.0, 1e-5);
-    printf("function: x^2sin^3x\n\tintegral[0, 1]: %lf; real integral: 0.11395\n", x);
-    assert(fabs(x -  0.11395) < 1e-5);
-    printf("Integral test: OK\n");
+    x = integral(it1, 0.0, 1.0, 1e-5);
+    printf("function: x^2sin^3x, EPS = 0.00001\n\tintegral[0, 1]: %.5lf; real integral: 0.11395\n", x);
+    if (fabs(x - 0.11395) < 1e-5)
+        printf("OK\n\n");
+    else
+        printf("WA\n\n");
+
+    printf("Integral test\n");
+    x = integral(it2, 0.5, 1.5, 1e-4);
+    printf("function: xsqrt(x^2+1), EPS = 0.0001\n\tintegral[0.5, 1.5]: %.4lf; real integral: 1.4871\n", x);
+    if (fabs(x - 1.4871) < 1e-4)
+        printf("OK\n\n");
+    else
+        printf("WA\n\n");
+
+    printf("Integral test\n");
+    x = integral(it3, 1.0, 2.7, 1e-3);
+    printf("function: log(x)/(x^2), EPS = 0.001\n\tintegral[0, 1]: %.3lf; real integral: 0.262\n", x);
+    if (fabs(x - 0.262) < 1e-3)
+        printf("OK\n\n");
+    else
+        printf("WA\n\n");
     return ;
 };
 
